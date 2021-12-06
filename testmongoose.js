0dc6346a60bb4fbe00cd17a58c6fb5ab3f84e064
zxp <111.com>
2021-12-05 16:40:41 -0800

    2th commit

--- testmongoose.js
+++ testmongoose.js
@@ -3,7 +3,8 @@ mongoose.connect('mongodb://localhost/test');
 const schema = {
     name: String,
     age: Number,
-    health: String
+    health: String,
+    hobby:String
 }
 const mydata = mongoose.model('Cat', schema);
 
